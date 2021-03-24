f78abf35a5356ea3b8a13b69af2e1443a10ab2ed
Roshini-rose <roshiniramesh763@gmail.com>
2021-03-23 23:23:08 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/rose/paytm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rose.paytm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pe_paytm.dart
@@ -0,0 +1,25 @@
+library pe_paytm;
+
+import 'package:flutter/material.dart';
+import 'package:paytm/order.dart';
+import 'package:paytm/payment_screen.dart';
+
+class PePaytm {
+  final String paymentURL;
+
+  PePaytm(
+      {this.paymentURL =
+      "https://us-central1-payments-testing-5fc15.cloudfunctions.net/customFunctions/payment"});
+
+  makePayment(context, {Order order}) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PaymentScreen(
+          order: order,
+          paymentURL: paymentURL,
+        ),
+      ),
+    );
+  }
+}
